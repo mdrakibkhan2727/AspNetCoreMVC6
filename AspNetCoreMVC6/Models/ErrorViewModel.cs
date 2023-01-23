54d9db4db712bd97eb589ede10b078b23d070aca
Md Rakib Khan <mdrakibkhan2727@gmail.com>
2023-01-23 21:57:45 +0600

    Add project files.

---
+++ AspNetCoreMVC6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreMVC6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
